7078fb9dafbcf4bd9d89d66bf6a4c2c72776f397
Victor Alarcon <valfonsob98@gmail.com>
2022-07-02 19:45:20 -0400

    Fix: Corrección del codigo
    Se corrigió el dato documento en la entidad persona.

--- HospiEnCasa.App/HospiEnCasa.App.Persistencia/Models/Enfermero.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Persistencia/Models/Enfermero.cs
@@ -15,9 +15,9 @@ namespace HospiEnCasa.App.Persistencia.Models
         public int IdEnfer { get; set; }
         public string Especialidad { get; set; }
         public string Registro { get; set; }
-        public string DocumentoPersona { get; set; }
+        public int IdPersona { get; set; }
 
-        public virtual Persona DocumentoPersonaNavigation { get; set; }
+        public virtual Persona IdPersonaNavigation { get; set; }
         public virtual ICollection<Asignado> Asignados { get; set; }
     }
 }
