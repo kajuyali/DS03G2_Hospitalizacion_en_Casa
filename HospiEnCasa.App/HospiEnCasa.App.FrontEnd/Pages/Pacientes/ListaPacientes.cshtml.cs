fe48f0d57a4a8e53713e97dc1df1f15c620ff083
VicAlar <77180750+VicAlar@users.noreply.github.com>
2022-07-17 11:48:32 -0400

    Consulta de la lista pacientes
    Funcionalidad para mostrar los pacientes en una lista.

--- HospiEnCasa.App/HospiEnCasa.App.FrontEnd/Pages/Pacientes/ListaPacientes.cshtml.cs
+++ HospiEnCasa.App/HospiEnCasa.App.FrontEnd/Pages/Pacientes/ListaPacientes.cshtml.cs
@@ -5,20 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using HospiEnCasa.App.Persistencia.Models;
+using HospiEnCasa.App.Persistencia.AppRepositorios;
 
 namespace HospiEnCasa.App.FrontEnd.Pages.Pacientes
 {
     public class ListaPacientes : PageModel
     {
         private readonly ILogger<ListaPacientes> _logger;
-
-        public ListaPacientes(ILogger<ListaPacientes> logger)
+        private readonly IRepositorioPaciente repositorioPaciente;
+        public IEnumerable<PacientesPer> Pacientes { get; set; }
+        public ListaPacientes(ILogger<ListaPacientes> logger, IRepositorioPaciente repositorioPaciente)
         {
             _logger = logger;
+            this.repositorioPaciente = repositorioPaciente;
         }
 
         public void OnGet()
         {
+            Pacientes = repositorioPaciente.ObtenerPacientes();
         }
     }
 }
