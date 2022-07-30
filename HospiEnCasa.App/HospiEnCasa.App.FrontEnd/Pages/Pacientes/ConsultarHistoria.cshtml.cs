964f6359e9e688206a15bd7ed2d085b71337cdcf
VicAlar <77180750+VicAlar@users.noreply.github.com>
2022-07-30 14:56:08 -0400

    Consultar las sugerencias de un paciente como medico

---
+++ HospiEnCasa.App/HospiEnCasa.App.FrontEnd/Pages/Pacientes/ConsultarHistoria.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using HospiEnCasa.App.Persistencia.Models;
+using HospiEnCasa.App.Persistencia.AppRepositorios;
+
+namespace HospiEnCasa.App.FrontEnd.Pages.Pacientes
+{
+    public class ConsultarHistoria : PageModel
+    {
+        private readonly ILogger<ConsultarHistoria> _logger;
+        private readonly IRepositorioHistorium repositorioHistorium;
+        private readonly IRepositorioSugerencia repositorioSugerencia;
+        private readonly IRepositorioPaciente repositorioPaciente;
+        public IEnumerable<Historium> RegistroHistoriaPaciente { get; set; } // Registro Completo Historia Médica Paciente
+        public Persona Persona { get; set; }
+        public PacientesPer Paciente { get; set; }
+        public IEnumerable<Sugerencia> Sugerencias { get; set; }
+
+
+        public ConsultarHistoria(ILogger<ConsultarHistoria> logger, IRepositorioHistorium repositorioHistorium, IRepositorioSugerencia repositorioSugerencia, IRepositorioPaciente repositorioPaciente)
+        {
+            _logger = logger;
+            this.repositorioHistorium = repositorioHistorium;
+            this.repositorioSugerencia = repositorioSugerencia;
+            this.repositorioPaciente = repositorioPaciente;
+        }
+
+        public void OnGet(int IdPaciente)
+        {
+            Paciente = repositorioPaciente.ObtenerPacienteById(IdPaciente);
+            RegistroHistoriaPaciente = repositorioHistorium.ObtenerHistoriaPaciente(IdPaciente);
+            Sugerencias = repositorioSugerencia.ObtenerSugerencias(IdPaciente);
+        }
+    }
+}
